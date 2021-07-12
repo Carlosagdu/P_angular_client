ae4a47dc69a4e9016645dc1852b38b1de29586e5
Carlos Aguilar Duarte <car2275580@maricopa.edu>
2021-07-11 23:22:51 -0500

    Blog display english posts add

--- src/app/core/components/page-blog-list-sidebar/page-blog-list-sidebar.component.ts
+++ src/app/core/components/page-blog-list-sidebar/page-blog-list-sidebar.component.ts
@@ -1,17 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+
+interface Post {
+  id: string;
+  title: string;
+  content: string;
+  pictureName: string | null;
+  createdAt: string;
+}
 
 @Component({
-  selector: 'app-page-blog-list-sidebar',
-  templateUrl: './page-blog-list-sidebar.component.html',
-  styleUrls: ['./page-blog-list-sidebar.component.css']
+  selector: "app-page-blog-list-sidebar",
+  templateUrl: "./page-blog-list-sidebar.component.html",
+  styleUrls: ["./page-blog-list-sidebar.component.css"],
 })
 export class PageBlogListSidebarComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public posts: Post[];
 
   ngOnInit(): void {
+    this.fetchPosts().subscribe((response) => {
+      this.posts = response.map((item) => {
+        let createdAt = item.createdAt.substring(0, 10);
+        let id = item.id;
+        let title = item.title;
+        let content = item.content.substring(0, 150) + "...";
+        let pictureUrl = item.pictureName;
+        return { createdAt, id, title, content, pictureUrl };
+      });
+    });
   }
 
-
-
+  fetchPosts(): Observable<any> {
+    return this.http.get<any>("http://localhost:3000/posts/english");
+  }
 }
