cbde6e930c993947141b07423662edd3b1315865
Carlos Aguilar Duarte <car2275580@maricopa.edu>
2021-07-18 12:50:50 -0500

    Post images size and latest entries fetch

--- src/app/core/components/index-landing-two/index-landing-two.component.ts
+++ src/app/core/components/index-landing-two/index-landing-two.component.ts
@@ -1,16 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+
+interface Post {
+  id: string;
+  title: string;
+  content: string;
+  pictureName: string | null;
+  createdAt: string;
+  commentsNumber: number;
+}
 
 @Component({
-  selector: 'app-index-landing-two',
-  templateUrl: './index-landing-two.component.html',
-  styleUrls: ['./index-landing-two.component.css']
+  selector: "app-index-landing-two",
+  templateUrl: "./index-landing-two.component.html",
+  styleUrls: ["./index-landing-two.component.css"],
 })
 export class IndexLandingTwoComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public posts: Post[];
 
   ngOnInit(): void {
+    this.fetchLatestPost().subscribe(
+      (response) => {
+        this.posts = response.map((item) => {
+          let createdAt = item.createdAt.substring(0, 10);
+          let id = item.id;
+          let title = item.title;
+          let content = item.content.substring(0, 200) + "...";
+          let pictureUrl = item.pictureName;
+          let commentsNumber = item.postLanguage.comments.length;
+          return {
+            createdAt,
+            id,
+            title,
+            content,
+            pictureUrl,
+            commentsNumber,
+          };
+        });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
-
+  fetchLatestPost(): Observable<any> {
+    return this.http.get<any>("http://localhost:3000/posts/latest");
+  }
 }
