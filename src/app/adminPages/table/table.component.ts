eda2301193924b4515930781e5ce7d6fb2e228e0
Carlos Aguilar Duarte <car2275580@maricopa.edu>
2021-07-24 01:17:07 -0500

    Actions buttons

--- src/app/adminPages/table/table.component.ts
+++ src/app/adminPages/table/table.component.ts
@@ -1,26 +1,71 @@
 import { HttpClient } from "@angular/common/http";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Type } from "@angular/core";
 import { Observable } from "rxjs";
+import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
-declare interface TableData {
-  headerRow: string[];
-  dataRows: string[][];
+@Component({
+  selector: "ngbd-modal-confirm-autofocus",
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title" id="modal-title">Profile deletion</h4>
+      <button
+        type="button"
+        class="close"
+        aria-label="Close button"
+        aria-describedby="modal-title"
+        (click)="modal.dismiss('Cross click')"
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <p>
+        <strong
+          >Are you sure you want to delete
+          <span class="text-primary">"John Doe"</span> profile?</strong
+        >
+      </p>
+      <p>
+        All information associated to this user profile will be permanently
+        deleted.
+        <span class="text-danger">This operation can not be undone.</span>
+      </p>
+    </div>
+    <div class="modal-footer">
+      <button
+        type="button"
+        class="btn btn-outline-secondary"
+        (click)="modal.dismiss('cancel click')"
+      >
+        Cancel
+      </button>
+      <button
+        type="button"
+        ngbAutofocus
+        class="btn btn-danger"
+        (click)="modal.close('Ok click')"
+      >
+        Ok
+      </button>
+    </div>
+  `,
+})
+export class NgbdModalConfirmAutofocus {
+  constructor(public modal: NgbActiveModal) {}
 }
 
-interface Post {
-  id: string;
-  title: string;
-  content: string;
-  pictureUrl: string | null;
-  createdAt: string;
-}
+const MODALS: { [name: string]: Type<any> } = {
+  autofocus: NgbdModalConfirmAutofocus,
+};
 
 @Component({
   selector: "table-cmp",
   templateUrl: "table.component.html",
 })
 export class TableComponent implements OnInit {
-  constructor(private http: HttpClient) {}
+  withAutofocus = `<button type="button" ngbAutofocus class="btn btn-danger"
+      (click)="modal.close('Ok click')">Ok</button>`;
+  constructor(private http: HttpClient, private _modalService: NgbModal) {}
   public tableData1: TableData;
   public tableData2: TableData;
 
@@ -46,4 +91,26 @@ export class TableComponent implements OnInit {
   fetchPosts(): Observable<Post[]> {
     return this.http.get<any>("http://localhost:3000/posts/english");
   }
+
+  displayMessage(id: string) {
+    console.log(id);
+  }
+
+  open(name: string, id: string) {
+    this._modalService.open(MODALS[name]);
+    console.log(id);
+  }
+}
+
+declare interface TableData {
+  headerRow: string[];
+  dataRows: string[][];
+}
+
+interface Post {
+  id: string;
+  title: string;
+  content: string;
+  pictureUrl: string | null;
+  createdAt: string;
 }
